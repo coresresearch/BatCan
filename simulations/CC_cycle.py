1cd25591858cb7d22b0dcd3fa4cb6622eb113cb1
decaluwe <steven.decaluwe@gmail.com>
2021-06-16 10:01:34 -0600

    Document  CC_cycle.py

--- simulations/CC_cycle.py
+++ simulations/CC_cycle.py
@@ -18,29 +18,36 @@ import cantera as ct
 from scikits.odes.dae import dae
 
 def run(SV_0, an, sep, ca, algvars, params):
-    # Determine the current to run at. 'calc_current' is defined below.
+    """ 
+    Run the simulation
+    """
+    # Determine the current to run at, and the time to fully charge/discharge. 
+    # 'calc_current' is defined below.
     current, t_final = calc_current(params['simulation'], an, ca)
+
+    # Store the location of all algebraic variables.
     params['algvars'] = algvars
+
     # Figure out which steps and at what currents to run the model. This 
     # returns a tuple of 'charge' and 'discharge' steps, and a tuple with a 
     # current for each step.
     steps, currents = setup_cycles(params['simulation'], current)
 
     # This function checks to see if certain limits are exceeded which will 
-    # terminate the model run:
+    # terminate the simulation:
     def terminate_check(t, SV, SVdot, return_val, inputs):
         return_val[0] = ca.voltage_lim(SV, ca, params['simulation']
                 ['phi-cutoff-lower'])
         return_val[1] = ca.voltage_lim(SV, ca, params['simulation']
                 ['phi-cutoff-upper'])
 
-    # Set up the solver:
+    # Set up the differential algebraic equation (dae) solver:
     options =  {'user_data':(an, sep, ca, params), 'rtol':1e-8, 'atol':1e-11, 
             'algebraic_vars_idx':algvars, 'first_step_size':1e-18, 
             'rootfn':terminate_check, 'nr_rootfns':2}
     solver = dae('ida', residual, **options)
 
-    # Go through the current steps and run the model for each current:
+    # Go through the current steps and integrate for each current:
     for i, step in enumerate(steps):
         print('Step ',int(i),': ',step,'...\n')
 
@@ -53,7 +60,7 @@ def run(SV_0, an, sep, ca, algvars, params):
         SV_0 = an.make_alg_consistent(SV_0, an, sep, ca, params)
         SV_0 = sep.make_alg_consistent(SV_0, an, sep, ca, params)
         
-        # This creates an initial residual vector and runs the integrator:
+        # Create an initial array of time derivatives and runs the integrator:
         SVdot_0 = np.zeros_like(SV_0)
         solution = solver.solve(t_out, SV_0, SVdot_0)
 
@@ -62,7 +69,6 @@ def run(SV_0, an, sep, ca, algvars, params):
 
         # Append the current data array to any preexisting data, for output.  
         # If this is the first step, create the output data array.
-        
         if i: # Not the first step. 'data_out' already exists:
             # Stack the times, the current at each time step, and the solution 
             # vector at each time step into a single data array.
@@ -84,9 +90,10 @@ def run(SV_0, an, sep, ca, algvars, params):
     return data_out
 
 def calc_current(params, an, ca):
-    # Calculates the external current from the user inputs.  If a C-rate is 
-    # given, calculate the battery capacity and convert this to a current.  If 
-    # i_ext is given, convert the units to A/m2.
+    """
+    Calculates the external current from the user inputs.  If a C-rate is given, calculate the battery capacity and convert this to a current.  If 
+    i_ext is given, convert the units to A/m2.
+    """
 
     # Battery capacity is the lesser of the anode and cathode capacities. It is 
     # required for determining the simulation time.
@@ -106,7 +113,7 @@ def calc_current(params, an, ca):
             # Convert i_ext to a float:
             i_ext = float(i_ext)
 
-            # Read the units and convert i_ext as necessary:
+            # Read the units and convert i_ext to A/m2 as necessary:
             i_units, A_units = units.split('/')
             if i_units=="mA":
                 i_ext *= 0.001
@@ -115,7 +122,7 @@ def calc_current(params, an, ca):
             if A_units=="cm2":
                 i_ext *= 10000
             
-    elif params['C-rate'] is not None: # User specified a C-rate
+    elif params['C-rate'] is not None: # User specified a C-rate, but not i_ext:
         i_ext = cap*params['C-rate']
 
     else:
@@ -129,8 +136,10 @@ def calc_current(params, an, ca):
     return i_ext, t_final
 
 def setup_cycles(params, current):
-    # Setup a tuple representing steps in the requested charge-discharge cycles.
-    # Also create a tuple of currents, one for each step.
+    """
+    Set up a tuple representing steps in the requested charge-discharge cycles.
+    Also create a tuple of currents, one for each step.
+    """
     steps = ()
     currents = ()
 
@@ -154,10 +163,12 @@ def setup_cycles(params, current):
     return steps, currents
 
 def residual(t, SV, SVdot, resid, inputs):
-    # Call the individual component residual functions, which implement 
-    # governing equations to solve for the state at any given time step. 
-    # Nothing is returned by this function. It merely needs to set the value of 
-    # 'SVdot':
+    """
+    Call the individual component residual functions, which implement 
+    governing equations to solve for the state at any given time step. 
+    Nothing is returned by this function. It merely needs to set the value of 
+    'resid':
+    """
     an, sep, ca, params = inputs
 
     # Call residual functions for anode, separator, and cathode. Assemble them 
@@ -169,33 +180,54 @@ def residual(t, SV, SVdot, resid, inputs):
     resid[ca.SVptr['residual']] = ca.residual(SV, SVdot, ca, sep, an, params)
 
 def output(solution, an, sep, ca, params):
-    # Prepare and save any output data to the correct location. Prepare, 
-    # create, and save any figures relevant to constant-current cycling.
+    """
+    Prepare and save any output data to the correct location. Prepare, 
+    create, and save any figures relevant to constant-current cycling.
+    """
     #TODO #17
     import matplotlib.pyplot as plt 
 
     # Calculate cell potential:   
     phi_ptr = 2+ca.SV_offset+int(ca.SVptr['phi_ed'][:])
     phi_elyte_ptr = np.add(sep.SV_offset+(sep.SVptr['phi'][:]), 2)
-
+ 
+    # Temporary flag for Li metal anode:
+    i_Li = 1
+    
     # Create figure:
     lp = 30 #labelpad
-    nplots = 4
+    # Number of subplots:
+    nplots = 3 + i_Li
+
+    # Initialize the figure:
     fig, axs = plt.subplots(nplots,1, sharex=True, 
             gridspec_kw = {'wspace':0, 'hspace':0})
+    
+   
+    # Axis 1: Current vs. capacity
     axs[0].plot(solution[0,:]/3600, 1000*solution[1,:]/10000)
     axs[0].set_ylabel('Current Density \n (mA/cm$^2$)',labelpad=lp-25)
-    axs[1].plot(solution[0,:]/3600, solution[phi_ptr,:])#V_cell)
+    # Axis 2: Charge/discharge potential vs. capacity.
+    axs[1].plot(solution[0,:]/3600, solution[phi_ptr,:])
     axs[1].set_ylabel('Cell Potential \n(V)',labelpad=lp)
-    axs[2].plot(solution[0,:]/3600, 1e6*solution[2+int(an.SVptr['thickness'])])
-    axs[2].set_ylabel('Anode Thickness \n($\mu$m)', labelpad=lp-10)
-    axs[2].set(xlabel='Time (h)')
-
+    # Axis 3: Separator electric potential vs. capacity.
     for j in np.arange(sep.n_points):
-        axs[3].plot(solution[0,:]/3600, solution[phi_elyte_ptr[j],:])
+        axs[2].plot(solution[0,:]/3600, solution[phi_elyte_ptr[j],:])
     
+    # Optional axis 4, For dense Li anode: anode thickness:
+    if i_Li:
+        axs[nplots-1].plot(solution[0,:]/3600, 
+            1e6*solution[2+int(an.SVptr['thickness'])])
+        axs[nplots-1].set_ylabel('Anode Thickness \n($\mu$m)', labelpad=lp-10)
+        axs[nplots-1].set(xlabel='Time (h)')
+
+    # Format axis ticks:
     for i in range(nplots):
         axs[i].tick_params(axis="x",direction="in")
         axs[i].tick_params(axis="y",direction="in")
+    
+    # Trim down whitespace:
     fig.tight_layout()
+
+    # Save figure:
     plt.savefig('output.pdf')
