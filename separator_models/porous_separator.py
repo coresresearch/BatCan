0a5980fbd660d4a16e0fe82a461d9e79ef8fcefe
decaluwe <steven.decaluwe@gmail.com>
2021-12-14 08:44:06 -0700

    Remove odd conditional in porous separator plotting.

--- separator_models/porous_separator.py
+++ separator_models/porous_separator.py
@@ -258,13 +258,12 @@ class separator():
         axs[ax_offset+1].plot(solution[0,:]/3600, Ck_elyte_an[an.index_Li,:],
             label="an interface")
 
-        if 1:
-            Ck_elyte_sep_ptr = np.add(self.SV_offset+self.SVptr['C_k_elyte'],2)
-            for j in np.arange(self.n_points):
-                axs[ax_offset+1].plot(solution[0,:]/3600, 
-                    solution[Ck_elyte_sep_ptr[j, self.index_Li],:], 
-                    label="separator "+str(j+1))
-                    
+        Ck_elyte_sep_ptr = np.add(self.SV_offset+self.SVptr['C_k_elyte'],2)
+        for j in np.arange(self.n_points):
+            axs[ax_offset+1].plot(solution[0,:]/3600, 
+                solution[Ck_elyte_sep_ptr[j, self.index_Li],:], 
+                label="separator "+str(j+1))
+
         for j in np.arange(ca.n_points):
             Ck_elyte_ca = solution[ca.SV_offset+ca.SVptr['C_k_elyte'][j]+2,:]
             axs[ax_offset+1].plot(solution[0,:]/3600, 
