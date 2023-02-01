9da6a97880858ce305f096febaf1237428aac3e8
decaluwe <steven.decaluwe@gmail.com>
2023-01-31 22:31:15 -0700

    Update indexing of elyte Li species
    Li_elyte index updated to disambiguate from Li_ed index.

--- separator_models/porous_separator.py
+++ separator_models/porous_separator.py
@@ -269,8 +269,8 @@ class separator():
 
         # Axis 5: Li+ concentration:
         Ck_elyte_an = solution[an.SVptr['C_k_elyte'][0]+SV_offset,:]
-        axs[ax_offset+1].plot(solution[0,:]/3600, Ck_elyte_an[an.index_Li,:],
-            label="an interface")
+        axs[ax_offset+1].plot(solution[0,:]/3600, 
+            Ck_elyte_an[an.index_Li_elyte,:], label="an interface")
 
         Ck_elyte_sep_ptr = \
             np.add(self.SV_offset+self.SVptr['C_k_elyte'],SV_offset)
