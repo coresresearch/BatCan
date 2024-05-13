24fffdf85b3ce3bbfb39eeea50af6cadefa2d83e
decaluwe <steven.decaluwe@gmail.com>
2024-05-13 17:37:39 -0600

    More efficient bandwidth calculator.
    Replaced previous double loop (NxN) with a single-pass bandwidth
    calculator.

--- submodels/bandwidth.py
+++ submodels/bandwidth.py
@@ -6,8 +6,9 @@ import numpy as np
 def calc_bandwidth(SV_0, an, sep, ca, params):
     # Calculate size N of Jacobian
     N = np.size(SV_0)
-    lband = 1
-    uband = 1
+    lband = 0
+    uband = 0
+
     SVdot = np.zeros_like(SV_0)
     params['i_ext'] = 0
 
@@ -22,16 +23,23 @@ def calc_bandwidth(SV_0, an, sep, ca, params):
         resid_i[ca.SVptr['electrode']] = ca.residual(0, SV, SVdot, sep, an, params)
         return resid_i
 
+    jac = np.zeros([N, N])
     resid_0 = calc_resid(SV_0)
+
     for i in range(N):
-        for j in range(N):
-            dSV = np.copy(SV_0)
-            dSV[j] = 1.01*SV_0[j]
-            dF = resid_0 - calc_resid(dSV)
-            if abs(dF[i]) > 0:
-                if j > i and abs(i - j) > uband:
-                    uband = abs(i - j)
-                elif i > j and abs(i - j) > lband:
-                    lband = abs(i - j)
+        dSV = np.copy(SV_0)
+        dSV[i] = 1.01 * (SV_0[i] + 0.01)
+        dF = resid_0 - calc_resid(dSV)
+        jac[:,i] += dF
+    
+    # Scan rows for max lband/uland
+    for i in range(N):
+        l_inds = np.where(abs(jac[i, :i]) > 0.)[0]
+        if len(l_inds) >= 1 and i - l_inds[0] > lband:
+            lband = i - l_inds[0]
+
+        u_inds = i + np.where(abs(jac[i, i:]) > 0.)[0]
+        if len(u_inds) >= 1 and u_inds[-1] - i > uband:
+            uband = u_inds[-1] - i
 
     return lband, uband
